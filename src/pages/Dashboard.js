764344306477fb79e565fa2c3c2d604118a053e9
Rivindu2 <89679753+Rivindu2@users.noreply.github.com>
2022-04-26 16:40:06 +0530

    usermanagment get

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -4,7 +4,7 @@ import DashboardContent from '../components/Dashboard/DashboardContent'
 import DashboardSidebar from '../components/Dashboard/DashboardSidebar'
 
 const Dashboard = () => {
-  const userType = 'supervisor'
+  const userType = 'Admin'
   const [activeTab, setActiveTab] = useState(0)
   const [openSideBar, setOpenSideBar] = useState(false)
 
