a64c2d706a00a493eb4ffb6d93e69d68250055b1
BhanukaAGH <it20009472@my.sliit.lk>
2022-04-26 13:43:46 +0530

    add all dependencies

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const App = () => {
-  return <h1 className='text-gray-800 text-xl'>Hello world!</h1>
+  return <h1 className='text-xl text-gray-800'>Hello world!</h1>
 }
 
 export default App
