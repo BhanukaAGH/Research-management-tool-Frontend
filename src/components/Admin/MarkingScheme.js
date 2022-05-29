b8962c5e9eb908ddb661c9e3f0beedc3ab1511da
BhanukaAGH <it20009472@my.sliit.lk>
2022-05-29 14:18:46 +0530

    rename subComponents folder

--- src/components/Admin/MarkingScheme.js
+++ src/components/Admin/MarkingScheme.js
@@ -1,59 +1,59 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { confirm } from "react-confirm-box";
-import CreateMS from "./Sub components/CreateMS";
-import AddCriteria from "./Sub components/addCriteria";
-import { useSnackbar } from "notistack";
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { confirm } from 'react-confirm-box'
+import CreateMS from './subComponents/CreateMS'
+import AddCriteria from './subComponents/addCriteria'
+import { useSnackbar } from 'notistack'
 
 const MarkingScheme = () => {
-  const { enqueueSnackbar } = useSnackbar();
+  const { enqueueSnackbar } = useSnackbar()
 
-  const [data, setData] = useState([]);
-  const [clickCreate, setclickCreate] = useState(false); //state to open create MS comonent
-  const [clickCriteria, setclickCriteria] = useState(false); //state to open create criteria comonent
-  const [ms, setms] = useState(null); //set markscheme id
+  const [data, setData] = useState([])
+  const [clickCreate, setclickCreate] = useState(false) //state to open create MS comonent
+  const [clickCriteria, setclickCriteria] = useState(false) //state to open create criteria comonent
+  const [ms, setms] = useState(null) //set markscheme id
 
   const cardList = () => {
-    axios.get("/api/v1/markscheme/get").then((json) => {
-      setData(json.data);
-      console.log(json.data);
-    });
-  };
+    axios.get('/api/v1/markscheme/get').then((json) => {
+      setData(json.data)
+      console.log(json.data)
+    })
+  }
 
   useEffect(() => {
-    cardList();
-  }, []);
+    cardList()
+  }, [])
 
   async function HandleDelete(id) {
-    console.log("Delete button works", id);
+    console.log('Delete button works', id)
 
     const result = await confirm(
-      "Are you sure you want to remove this MarkScheme"
-    );
+      'Are you sure you want to remove this MarkScheme'
+    )
     if (result) {
-      const url = `/api/v1/markscheme/del/${id}`;
+      const url = `/api/v1/markscheme/del/${id}`
 
       axios
         .delete(url)
         .then((response) => {
-          console.log(response);
-          enqueueSnackbar(response.data.msg, { variant: "success" });
+          console.log(response)
+          enqueueSnackbar(response.data.msg, { variant: 'success' })
           //to refresh the data
-          cardList();
+          cardList()
         })
         .catch(function (error) {
-          console.log("error", error);
+          console.log('error', error)
 
-          enqueueSnackbar(error.response.data.msg, { variant: "error" });
-        });
+          enqueueSnackbar(error.response.data.msg, { variant: 'error' })
+        })
     } else {
       //console.log("users not removed");
-      enqueueSnackbar("Mark Scheme Not Deleted", { variant: "info" });
+      enqueueSnackbar('Mark Scheme Not Deleted', { variant: 'info' })
     }
   }
 
   function test() {
-    console.log("button works", data);
+    console.log('button works', data)
   }
 
   const renderCard = () => {
@@ -61,45 +61,45 @@ const MarkingScheme = () => {
       //display details
       return (
         <div key={index}>
-          <div className="block max-w-sm rounded-lg border border-gray-200 bg-white p-6 shadow-md hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+          <div className='block max-w-sm rounded-lg border border-gray-200 bg-white p-6 shadow-md hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700'>
             <a
               onClick={() => {
-                setms(ms);
-                setclickCriteria(true);
+                setms(ms)
+                setclickCriteria(true)
               }}
             >
-              <h5 className="mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white">
+              <h5 className='mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white'>
                 {ms.markSchemeName}
               </h5>
 
-              <ul className="list-none">
-                <li className="text-base font-normal text-gray-700 dark:text-gray-400">
-                  {ms.schemeType}{" "}
+              <ul className='list-none'>
+                <li className='text-base font-normal text-gray-700 dark:text-gray-400'>
+                  {ms.schemeType}{' '}
                 </li>
-                <li className="text-base font-normal text-gray-700 dark:text-gray-400">
+                <li className='text-base font-normal text-gray-700 dark:text-gray-400'>
                   {ms.Description}
                 </li>
-                <li className="text-base font-normal text-gray-700 dark:text-gray-400"></li>
+                <li className='text-base font-normal text-gray-700 dark:text-gray-400'></li>
               </ul>
               <br />
             </a>
             <button
-              type="button"
+              type='button'
               onClick={() => {
-                HandleDelete(ms._id);
+                HandleDelete(ms._id)
               }}
-              className="mr-2 mb-2 rounded-full bg-red-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+              className='mr-2 mb-2 rounded-full bg-red-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900'
             >
               Remove
             </button>
           </div>
         </div>
-      );
-    });
-  };
+      )
+    })
+  }
 
   return (
-    <div className="h-full w-full overflow-auto p-5">
+    <div className='h-full w-full overflow-auto p-5'>
       MarkingScheme Managment
       <br />
       {clickCriteria && (
@@ -111,37 +111,37 @@ const MarkingScheme = () => {
       )}
       {!clickCriteria && (
         <div>
-          <div className="flex items-center justify-center">
+          <div className='flex items-center justify-center'>
             <button
-              type="button"
-              className="mr-2 mb-2 rounded-lg bg-[#e2a500]  px-5 py-2.5 text-sm font-medium text-white hover:bg-yellow-500 focus:outline-none"
+              type='button'
+              className='mr-2 mb-2 rounded-lg bg-[#e2a500]  px-5 py-2.5 text-sm font-medium text-white hover:bg-yellow-500 focus:outline-none'
               onClick={() => {
-                setclickCreate(true);
+                setclickCreate(true)
               }}
             >
               Create Mark Scheme
             </button>
           </div>
-          <div className="justify-left flex items-center">
+          <div className='justify-left flex items-center'>
             <button
-              className="mr-2 bg-yellow-100 px-2.5 py-0.5 text-xs font-semibold text-yellow-800 hover:bg-yellow-200  dark:bg-yellow-200 dark:text-yellow-800 dark:hover:bg-yellow-300"
+              className='mr-2 bg-yellow-100 px-2.5 py-0.5 text-xs font-semibold text-yellow-800 hover:bg-yellow-200  dark:bg-yellow-200 dark:text-yellow-800 dark:hover:bg-yellow-300'
               onClick={cardList}
             >
               refresh Cards
             </button>
 
             <br />
-            <p className="text-sm text-red-500">
+            <p className='text-sm text-red-500'>
               Click On MarkScheme to add criteria
             </p>
           </div>
           <br />
-          <div className="grid grid-cols-4 gap-4">{renderCard()}</div>
+          <div className='grid grid-cols-4 gap-4'>{renderCard()}</div>
           {clickCreate && <CreateMS setclickCreate={setclickCreate} />}
         </div>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default MarkingScheme;
+export default MarkingScheme
--- src/components/Admin/StudentGroups.js
+++ src/components/Admin/StudentGroups.js
@@ -1,87 +1,86 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { confirm } from "react-confirm-box";
-import Allocate from "./Sub components/Allocate";
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import Allocate from './subComponents/Allocate'
 
 const StudentGroups = () => {
-  const [data, setData] = useState([]); //get all student groups
-  const [allocate, setAllocate] = useState(false); //open allocatewindow
-  const [groupID, setGroupID] = useState(""); //set group id of selected group
+  const [data, setData] = useState([]) //get all student groups
+  const [allocate, setAllocate] = useState(false) //open allocatewindow
+  const [groupID, setGroupID] = useState('') //set group id of selected group
 
   //get student group details
   const tableList = () => {
-    axios.get("/api/v1/student/getgroups").then((json) => setData(json.data));
-  };
+    axios.get('/api/v1/student/getgroups').then((json) => setData(json.data))
+  }
 
   useEffect(() => {
-    tableList();
-  }, []);
+    tableList()
+  }, [])
   const renderTable = () => {
     return data.map((user, index) => {
       //display details
       return (
         <tr
-          className="border-b bg-white hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-[#3a454b]"
+          className='border-b bg-white hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-[#3a454b]'
           key={index}
         >
-          <td className="w-4 p-4"></td>
-          <td className="whitespace-nowrapont-medium text-gray-900 dark:text-white">
+          <td className='w-4 p-4'></td>
+          <td className='whitespace-nowrapont-medium text-gray-900 dark:text-white'>
             {user.groupID}
           </td>
-          <td className="whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white">
+          <td className='whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white'>
             <ul>
               <li>{user.leader.name}</li>
               <li>{user.leader.regNo}</li>
               <li>{user.leader.email}</li>
             </ul>
           </td>
-          <td className="whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white">
+          <td className='whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white'>
             <ul>
               <li>{user.member2.name}</li>
               <li>{user.member2.regNo}</li>
               <li>{user.member2.email}</li>
             </ul>
           </td>
-          <td className="whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white">
+          <td className='whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white'>
             <ul>
               <li>{user.member3.name}</li>
               <li>{user.member3.regNo}</li>
               <li>{user.member3.email}</li>
             </ul>
           </td>
-          <td className="whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white">
+          <td className='whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white'>
             <ul>
               <li>{user.member4.name}</li>
               <li>{user.member4.regNo}</li>
               <li>{user.member4.email}</li>
             </ul>
           </td>
-          <td className="whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white">
+          <td className='whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white'>
             {user.Panelmember}
           </td>
-          <td className="px-6 py-4 text-right">
+          <td className='px-6 py-4 text-right'>
             <a
-              className="font-medium text-[#e2a500] hover:underline dark:text-[#e2a500]"
+              className='font-medium text-[#e2a500] hover:underline dark:text-[#e2a500]'
               onClick={() => {
-                setGroupID(user._id);
-                setAllocate(true);
+                setGroupID(user._id)
+                setAllocate(true)
               }}
             >
               Allocate
             </a>
           </td>
         </tr>
-      );
-    });
-  };
+      )
+    })
+  }
 
   return (
-    <div className="h-full w-full overflow-auto p-5">
+    <div className='h-full w-full overflow-auto p-5'>
       StudentGroups
-      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-        <div className="justify-left flex items-center">
+      <div className='relative overflow-x-auto shadow-md sm:rounded-lg'>
+        <div className='justify-left flex items-center'>
           <button
-            className="mr-2 bg-yellow-100 px-2.5 py-0.5 text-xs font-semibold text-yellow-800 hover:bg-yellow-200  dark:bg-yellow-200 dark:text-yellow-800 dark:hover:bg-yellow-300"
+            className='mr-2 bg-yellow-100 px-2.5 py-0.5 text-xs font-semibold text-yellow-800 hover:bg-yellow-200  dark:bg-yellow-200 dark:text-yellow-800 dark:hover:bg-yellow-300'
             onClick={tableList}
           >
             refresh Table
@@ -89,34 +88,34 @@ const StudentGroups = () => {
         </div>
         <a onClick={tableList}>
           <table
-            id="myTable"
-            className="w-full text-left text-sm text-gray-500 dark:text-gray-400"
+            id='myTable'
+            className='w-full text-left text-sm text-gray-500 dark:text-gray-400'
           >
-            <thead className="bg-[#3a454b] text-xs uppercase text-[#e2a500] dark:bg-[#3a454b] dark:text-[#e2a500]">
+            <thead className='bg-[#3a454b] text-xs uppercase text-[#e2a500] dark:bg-[#3a454b] dark:text-[#e2a500]'>
               <tr>
-                <th scope="col" className="p-4">
-                  <div className="flex items-center"></div>
+                <th scope='col' className='p-4'>
+                  <div className='flex items-center'></div>
                 </th>
-                <th scope="col" className="px-2 py-1">
+                <th scope='col' className='px-2 py-1'>
                   Group ID
                 </th>
-                <th scope="col" className="px-6 py-3">
+                <th scope='col' className='px-6 py-3'>
                   Leader
                 </th>
-                <th scope="col" className="px-6 py-3">
+                <th scope='col' className='px-6 py-3'>
                   Member 1
                 </th>
-                <th scope="col" className="px-6 py-3">
+                <th scope='col' className='px-6 py-3'>
                   Member 2
                 </th>
-                <th scope="col" className="px-6 py-3">
+                <th scope='col' className='px-6 py-3'>
                   Member 3
                 </th>
-                <th scope="col" className="px-6 py-3">
+                <th scope='col' className='px-6 py-3'>
                   Panel_Member
                 </th>
-                <th scope="col" className="px-6 py-3">
-                  <span className="sr-only">Edit</span>
+                <th scope='col' className='px-6 py-3'>
+                  <span className='sr-only'>Edit</span>
                 </th>
               </tr>
             </thead>
@@ -127,7 +126,7 @@ const StudentGroups = () => {
       </div>
       {allocate && <Allocate groupID={groupID} setAllocate={setAllocate} />}
     </div>
-  );
-};
+  )
+}
 
-export default StudentGroups;
+export default StudentGroups
