797f15cf3cb3aba88e6975743641a1f2e60045df
Buddheesha Induwara <buddhishainduwara@gmail.com>
2022-06-01 16:55:16 +0530

    student Group updated

--- src/components/Student/StudentGroup.js
+++ src/components/Student/StudentGroup.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
-// import { useSelector } from 'react-redux'
-// import axios from 'axios'
+import { useSelector } from 'react-redux'
+import axios from 'axios'
 // import Spinner from '../Spinner'
 
 import StudentGroupDetails from './StudentGroup/StudentGroupDetails'
@@ -8,27 +8,20 @@ import AddStudentGroup from './StudentGroup/AddStudentGroup'
 
 const StudentGroup = () => {
   // const [loading, setLoading] = useState(false)
-  // const [groupData, setGroupData] = useState(null)
+  const [groupData, setGroupData] = useState(null)
   const [showRegister, setShowRegister] = useState(false)
 
-  // const { user } = useSelector((store) => store.auth)
+  const { user } = useSelector((store) => store.auth)
 
-  // const groupDetails = async () => {
-  //   setLoading(true)
-  //   try {
-  //     const response = await axios.get(
-  //       `${process.env.SERVER_BACKEND_URL}/api/v1/student/${user.userId}`
-  //     )
-  //     setGroupData(response.data)
-  //     console.log(groupData.groupID)
-  //   } catch (error) {
-  //     console.log(error.message)
-  //   }
-  //   setLoading(false)
-  // }
-  // useEffect(() => {
-  //   groupDetails()
-  // }, [])
+  const groupDetails = () => {
+    axios
+      .get(`${process.env.SERVER_BACKEND_URL}/api/v1/student/${user.userId}`)
+      .then((res) => setGroupData(res.data))
+  }
+
+  useEffect(() => {
+    groupDetails()
+  }, [])
 
   // if (loading) {
   //   return <Spinner />
@@ -37,19 +30,23 @@ const StudentGroup = () => {
   return (
     <div className='h-full w-full overflow-auto p-5'>
       <div className='flex flex-row space-x-10'>
-        <div> Student Group </div>
-
-        <div>
-          <button
-            className='w-2/12 rounded border-2 border-indigo-400 bg-gray-900 py-2 text-center font-bold text-indigo-400 focus:outline-none'
-            onClick={() => setShowRegister(!showRegister)}
-          >
-            Register
-          </button>
-        </div>
+        {!groupData && (
+          <div className='mb-4 flex w-full items-center justify-end'>
+            <div
+              className='hover:scale-102 flex max-w-fit cursor-pointer items-center space-x-2 rounded-md bg-gray-700 py-1 px-4 text-base text-slate-50 duration-500 hover:shadow-lg hover:shadow-gray-600'
+              onClick={() => setShowRegister(!showRegister)}
+            >
+              {showRegister ? 'Go Back' : ' Register Group'}
+            </div>
+          </div>
+        )}
       </div>
 
-      {showRegister ? <AddStudentGroup /> : <StudentGroupDetails />}
+      {showRegister ? (
+        <AddStudentGroup />
+      ) : (
+        <StudentGroupDetails groupData={groupData} />
+      )}
     </div>
   )
 }
--- src/components/Student/StudentGroup/StudentGroupDetails.js
+++ src/components/Student/StudentGroup/StudentGroupDetails.js
@@ -1,30 +1,17 @@
-import React, { useState, useEffect } from 'react'
-import { useSelector } from 'react-redux'
-import axios from 'axios'
+import React from 'react'
 
-const StudentGroupDetails = () => {
-  const [groupData, setGroupData] = useState(null)
-
-  const { user } = useSelector((store) => store.auth)
-
-  const groupDetails = () => {
-    axios
-      .get(`${process.env.SERVER_BACKEND_URL}/api/v1/student/${user.userId}`)
-      .then((res) => setGroupData(res.data))
-  }
-
-  useEffect(() => {
-    groupDetails()
-  }, [])
-
-  // if (groupData.leader) {
-  //   console.log(groupData.leader.email)
-  // }
-
+const StudentGroupDetails = ({ groupData }) => {
   if (groupData) {
     return (
       <div>
+        <div>
+          {' '}
+          Group Registration Status:{' '}
+          {groupData ? 'Registered' : 'Not Registered'}{' '}
+        </div>
+        <br />
         <div> Your Group ID : {groupData.groupID} </div>
+        <br />
         <div className='scrollbar max-h-full w-full overflow-auto scroll-smooth'>
           <table className='relative h-full w-full overflow-auto text-left text-sm text-gray-500'>
             <thead className='sticky top-0'>
